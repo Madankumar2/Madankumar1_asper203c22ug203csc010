66b3578cea4a548d8a4548aad4be237f6e8a70ad
Madankumar2 <144319859+Madankumar2@users.noreply.github.com>
2023-10-04 14:52:33 +0530

    Add files via upload

---
+++ challege unit-3/challenge .3.py
@@ -0,0 +1,15 @@
+def linearsearchproduct(productlist, targetproduct):
+ indices = []
+ 
+ for index, product in enumerate(productlist):
+     if product == targetproduct:
+       indices.append(index)
+ 
+ return indices
+ 
+products =["shoes", "boot", "Loafer", "Shoes", "sandal", "shoes"]
+target = "Shoes" 
+target2 ='apple'
+result = linearsearchproduct(products, target)
+print(result)
+ 
